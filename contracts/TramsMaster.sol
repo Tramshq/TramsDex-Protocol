3a2bd3d7de5d39a2ce87bf3b336f65b873cdd5d6
TramsDriver <TramsDriver@protonmail.com>
2020-10-28 05:06:26 +0530

    TRAMS Token Contract

--- contracts/TramsMaster.sol
+++ contracts/TramsMaster.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./SushiToken.sol";
+import "./TramsToken.sol";
 
 
 interface IMigratorChef {
